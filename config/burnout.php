dc0ccbb223a645de3847a3253af68b8b62f354be
Fabio Ivona <fabio.ivona@defstudio.it>
2020-11-03 14:16:41 +0100

    skips authentication and authorization exception log

--- config/burnout.php
+++ config/burnout.php
@@ -8,6 +8,8 @@ return [
     'delete_logs_older_than_days' => env('BURNOUT_LOGS_VALIDITY_DAYS', 15),
 
     'skip_exceptions' => [
+        \Illuminate\Auth\AuthenticationException::class,
+        \Illuminate\Auth\Access\AuthorizationException::class,
         \Illuminate\Validation\ValidationException::class,
         \Symfony\Component\HttpKernel\Exception\NotFoundHttpException::class,
     ]
